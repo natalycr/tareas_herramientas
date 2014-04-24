62627002e10d9e7dc3f7ac6211a1928beeee11b3
nataly <n.castillo1387@uniandes.edu.co>
2014-04-24 14:58:54 -0500

    EntregaFinalParticulaSol

--- castilloRuizgaravito-camargo/grafica.py
+++ castilloRuizgaravito-camargo/grafica.py
@@ -9,10 +9,11 @@ data = np.loadtxt("datosTrayectoria.dat")
 x = data[:,3]#datos de Numero de pasos
 y = data[:,4]# Distancias 
 
-py.plot (x, y, '-r', c='g')
+py.plot (x, y, '-r', c='g', label="Datos")
 py.title("$Grafica\ de\ pasos\ Vs\ distancia\ de\ la\ particula\ al\ centro$", fontsize=20)
 py.xlabel("$Pasos$", fontsize= 20)
 py.ylabel("$Distancia\ particula$", fontsize=20)
+py.legend(loc=2)
 
 py.show()
 py.savefig("Npasos-r.png")

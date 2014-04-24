7ab315ce70ae70beb630c2629fc4d83142b40941
nataly <n.castillo1387@uniandes.edu.co>
2014-04-24 14:34:47 -0500

    ajus

--- castilloRuizgaravito-camargo/marchaAleatoria.py
+++ castilloRuizgaravito-camargo/marchaAleatoria.py
@@ -25,13 +25,13 @@ ra.append(r)
 
 while r<=R:
     i=i+1
+    p=1 # distancia de cada  paso
 
-#    p=np.random.rand()*10
     #para la particula pasos de 1
     phi =np.random.rand()*math.pi
     theta=np.random.rand()*math.pi*2
 
-    p=1 # distancia de cada  paso
+    
     xi=p* np.sin(phi)* np.cos(theta) #posicion en x
     yi=p* np.sin(phi)* np.sin(theta) #posicion en y
     zi=p* np.cos(phi)#posicion en z
